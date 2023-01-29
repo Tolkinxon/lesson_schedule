ec2c9f292b4f1199d4ed74972103a7073db847fc
Tolkinxon <soliyevtolkinxon@gmail.com>
2023-01-29 18:23:57 +0500

    separataed all codes to other component folders

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import './App.css'
+import Counter from '../counter'
 
 function App() {
   return (
-    <div className="App">
-      {/* <h1>hello world</h1> */}
-    </div>
+  <>
+    <Counter />
+  </>
   )
 }
 
--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
 function Counter() {
+ 
+
   return (
     <>
-    <span><b>count:</b></span>
-    <span id="counter"></span>
-    <br />
-    <button className="btn btn-primary">increment++</button>
-    <button className="btn btn-danger">decrement--</button>
-    <button className="btn btn-info">randon</button>
+      <span>
+        <b>count: </b>
+      </span>
+      <br />
+      <button className="btn btn-primary" >increment++</button>
+      <button className="btn btn-danger">decrement--</button>
+      <button className="btn btn-info">random</button>
     </>
   )
 }
