5ccc7622e68220f3006618a8c359438628fb36a0
Tolkinxon <krosstony639@gmail.com>
2024-05-28 12:23:57 +0300

    addes react router dom library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './css/index.css'
 import Main from "./components/Main"
 import AddSchedule from './components/AddSchedule'
+import { Route, Routes } from 'react-router-dom'
 
 
 function App() {
   return (
     <>
-      {/* <Main /> */}
-      <AddSchedule /> 
+      <Routes>
+        <Route path='/' element={ <Main /> }/>
+        <Route path='/add-schedule' element={ <AddSchedule /> }/>
+      </Routes>
+      
+       
     </>
   )
 }
