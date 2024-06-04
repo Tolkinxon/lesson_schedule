ec37abb584a8195f95c0dfcf163f4365e893aaac
Tolkinxon <krosstony639@gmail.com>
2024-06-04 18:24:21 +0300

    some hours editing added

--- src/components/ConfigureTimeLessons.jsx
+++ src/components/ConfigureTimeLessons.jsx
@@ -1,11 +1,13 @@
 import ConfigureTimeLessonsItem from './ConfigureTimeLessonsItem';
 import { useEffect, useState } from 'react';
 import { useHttp }  from '../hooks/useHttp';
+import { retry } from '@reduxjs/toolkit/query';
 
 
 const ConfigureTimeLessons = () => {
 
     const [isOpenEditTimeSection, setIsOpenEditTimeSection] = useState(false)
+    const [changeIndex, setChangeIndex] = useState(0)
 
     const { request } = useHttp()
 
@@ -28,7 +30,16 @@ const ConfigureTimeLessons = () => {
        return <ConfigureTimeLessonsItem item={ item } idx={ idx } setIsOpenEditTimeSection={setIsOpenEditTimeSection}/>
     })
 
-    
+  
+    function generatorTimes(limitTime) {
+        const generatedTimes = []
+        for(let i = 0; i < limitTime; i++){
+            const time = (`${i}`.length == 2 ? `${i}`:`0${i}`)
+            generatedTimes.push(<li className='editing-time__item' key={i} >{ time }</li>)
+        }
+        return generatedTimes
+    }
+
 
     return ( 
         <>
@@ -47,20 +58,29 @@ const ConfigureTimeLessons = () => {
                     </ul>
                 </div>
 
-                <section className='editing-time' style={{display: isOpenEditTimeSection ? 'block':'none'}}>
+                <section className='editing-time' style={{display: isOpenEditTimeSection ? '':''}}>
                     <div className='container editing-time__container'>
                         <div className='editing-time__line'></div>
 
                         <p className='editing-time__text' onClick={() => setIsOpenEditTimeSection(prev => prev = !prev)}> Select </p>
 
-                        <div className='editing-time__wrapper'>
-                            <ul className='editing-time__start-list'>
+                        <div className='editing-time__wrapper' onClick={() => setChangeIndex(prev => prev -= 1)}>
+                            <div>
+                                <p className='editing-time__selected-for-start'>00:00</p>
+                                <div className='editing-time__start-list-wrapper'>
+                                    <ul className='editing-time__start-list-for-hour' >
+                                        { generatorTimes(24) }
+                                    </ul>
+                                    <ul className='editing-time__start-list-for-minute' >
+                                        { generatorTimes(60) }
+                                    </ul>
+                                </div>
+                            </div>
+                       
 
-                            </ul>
-
-                            <ul className='editing-time__end-list'>
-
-                            </ul>
+                            {/* <ul className='editing-time__end-list'>
+                            { generatorTimes(60) }
+                            </ul> */}
                         </div>
                     </div>
                 </section>
